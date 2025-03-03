8ad9f04f5474d81509233d4865ca03720786227a
cortexSouza <cortex.souza.oli07@gmail.com>
2025-03-03 17:28:53 -0300

    resolução de alguns exe.

--- Curso em vídeo/Aprendendo Python/ex019.py
+++ Curso em vídeo/Aprendendo Python/ex019.py
@@ -1,2 +1,10 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 
+from random import choice
+nam1 = input('Qual o nome do aluno: ')
+nam2 = input('Qual o nome do aluno: ')
+nam3 = input('Qual o nome do aluno: ')
+nam4 = input('Qual o nome do aluno: ')
+list = [nam1,nam2,nam3,nam4]
+escolhido = choice(list)
+print('O escolhido foi {}'.format(escolhido))
--- Curso em vídeo/Aprendendo Python/ex020.py
+++ Curso em vídeo/Aprendendo Python/ex020.py
@@ -1,2 +1,10 @@
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
+from random import shuffle
+nam1 = input('Nome do aluno: ')
+nam2 = input('Nome do aluno: ')
+nam3 = input('Nome do aluno: ')
+nam4 = input('Nome do aluno: ')
+lista = [nam1,nam2,nam3,nam4]
+shuffle(lista)
+print('A ordem de apresentação será: {}'.format(lista))
